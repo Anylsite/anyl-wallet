4bff10fcfc72c1dd1cccd3f716caf3e2f6c5cd3d
Jozef Henzl <popcorp86@gmail.com>
2018-08-31 14:30:07 +0200

    Added test for the RLP encoder

--- helpers/rlp_encoder.h
+++ helpers/rlp_encoder.h
@@ -8,8 +8,20 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+// the max size of header data in the RLP format is 8 bytes = uint64_max
+// since we use uint32_t as an input len, we don't need to allocate more than 4+1 bytes for the header
+#define HEADER_LEN  4+1  // max size of the header
+#define HEADER_DATA_MAXLEN 1<<(((HEADER_LEN-1)*8)-1) // max size of the data contained in an array header
+
 uint32_t encode_item(uint8_t* output, const uint8_t* input, uint32_t input_len);
 uint32_t encode_whole_header(uint8_t *header_output, uint32_t total_len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  /* RLP_ENCODER_H__ */
 
