494c31ddc4587117c68dec7e2c7b2ad0dec6d1e8
Jozef Henzl <popcorp86@gmail.com>
2018-09-25 11:44:46 +0200

    Enable linter for CI

--- helpers/rlp_encoder.h
+++ helpers/rlp_encoder.h
@@ -7,14 +7,19 @@
 #define RLP_ENCODER_H__
 
 #include <stdint.h>
+#include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 // the max size of header data in the RLP format is 8 bytes = uint64_max
 // since we use uint32_t as an input len, we don't need to allocate more than 4+1 bytes for the header
 #define HEADER_LEN  (4+1)  // max size of the header
 #define HEADER_DATA_MAXLEN 1<<(((HEADER_LEN-1)*8)-1) // max size of the data contained in an array header
+    //
+// return header size for the data of @input_len
+uint8_t header_len(size_t input_len);
 
 uint32_t encode_item(uint8_t* output, const uint8_t* input, uint32_t input_len);
 uint32_t encode_whole_header(uint8_t *header_output, uint32_t total_len);
