53e35c1b67f44201b250a945102561744a5c6678
Jozef Henzl <popcorp86@gmail.com>
2018-10-09 12:49:57 +0200

    Add trezor-crypto library
    - external lib definition for trezor-crypto which we'll most likely use

---
+++ cmake/trezor-crypto.cmake
@@ -0,0 +1,22 @@
+# clones & builds `trezor-crypto` library
+include(ExternalProject)
+
+set(mylib_build_dir ${CMAKE_CURRENT_BINARY_DIR}/trezor-crypto)
+set(mylib_source_dir ${CMAKE_CURRENT_BINARY_DIR}/thirdparty/trezor-crypto)
+
+ExternalProject_Add(
+  trezor-crypto
+
+  PREFIX     ${mylib_build_dir} # Root dir for entire project
+  GIT_REPOSITORY "git@github.com:trezor/trezor-crypto.git"
+  GIT_TAG "master"
+  
+  UPDATE_COMMAND ""
+  PATCH_COMMAND ${CMAKE_COMMAND} -E copy
+  "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/trezor-crypto/CMakeLists.txt" ${mylib_source_dir}/CMakeLists.txt
+  
+  SOURCE_DIR ${mylib_source_dir}
+  
+  TEST_COMMAND ""
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/install
+)
