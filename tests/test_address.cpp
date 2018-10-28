61bf49cfbc1bec4eb5a7b44aff2e964ff6ceebe0
Mohit Agnihotri <mohitkumar39@gmail.com>
2018-10-28 11:58:16 +0000

    Add correct testcase for the address_from_signature API

--- tests/test_address.cpp
+++ tests/test_address.cpp
@@ -23,11 +23,20 @@ TEST(TEST_ADDRESS, TEST_ADDRESS_RECOVERY)
     address_t _expected_addr;
     address_t _addr;
     uint8_t signature[65];
-    const char* _data_to_sign = "Hello world";
-    hextobin("30755ed65396facf86c53e6217c52b4daebe72aa4941d89635409de4c9c7f9466d4e9aaec7977f05e923889b33c0d0dd27d7226b6e6f56ce737465c5cfd04be400", signature, sizeof(signature));
-    hextobin("11f4d0A3c12e86B4b5F39B213F7E19D048276DAe", _expected_addr, sizeof(_expected_addr));
+    const char *_data_to_sign = "A";
+    signature_t sign = {{0u}, {0u}, 0u};
 
-    address_from_signature((uint8_t*)_data_to_sign, strlen(_data_to_sign), (const signature_t *)signature, &_addr);
+    hextobin("eac4a6600cb8b05876932ab90d4db031341f30649ac812021ead6fc90c604ce17c8dd6bb39a5545f75f3593b7db8f843d74ceac0bfa3696bbffeb11dda80db7b1c", signature, sizeof(signature));
+    hextobin("7e5f4552091a69125d5dfcb7b8c2659029395bdf", _expected_addr, sizeof(_expected_addr));
+
+
+    memcpy(&sign.r, signature, sizeof(uint256_t));
+    memcpy(&sign.s, signature+sizeof(uint256_t), sizeof(uint256_t));
+    sign.v = signature[64];
+
+    int status = address_from_signature((uint8_t*)_data_to_sign, strlen(_data_to_sign), &sign, &_addr);
+
+    ASSERT_TRUE(status == 0u);
     ASSERT_EQ(memcmp(_expected_addr, _addr, 20), 0);
 }
 
